92f91ef67cb0ce441f0d2b273be2cf9068065db9
AndyAWD <anandydy529@gmail.com>
2020-05-04 20:28:45 +0800

    feat: 建立BaseApplication

---
+++ app/src/main/java/tw/com/andyawd/colosseoduel/BaseApplication.kt
@@ -0,0 +1,25 @@
+package tw.com.andyawd.colosseoduel
+
+import android.app.Application
+import tw.com.andyawd.andyawdlibrary.AWDConstants
+import tw.com.andyawd.andyawdlibrary.AWDLog
+
+class BaseApplication : Application() {
+    init {
+        context = this
+    }
+
+    companion object {
+        private var context: BaseApplication? = null
+
+        fun context(): BaseApplication {
+            return context as BaseApplication
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AWDLog.setLogLevel(AWDConstants.LOG_VERBOSE)
+    }
+}
