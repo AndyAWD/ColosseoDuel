944a2b68670376c60670e3ae4308f115898f922d
AndyAWD <anandydy529@gmail.com>
2020-07-11 20:21:09 +0800

    feat: 修改主頁面選單名稱

--- app/src/main/java/tw/com/andyawd/colosseoduel/FragmentOpenListener.kt
+++ app/src/main/java/tw/com/andyawd/colosseoduel/FragmentOpenListener.kt
@@ -1,6 +1,6 @@
 package tw.com.andyawd.colosseoduel
 
 abstract class FragmentOpenListener {
-    open fun onOpenVideoRecordView() {}
-    open fun onOpenVideoListView() {}
+    open fun onOpenDuelScore() {}
+    open fun onOpenHistoryRecord() {}
 }
--- app/src/main/java/tw/com/andyawd/colosseoduel/MainActivity.kt
+++ app/src/main/java/tw/com/andyawd/colosseoduel/MainActivity.kt
@@ -1,23 +1,14 @@
 package tw.com.andyawd.colosseoduel
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import com.jakewharton.rxbinding3.view.clicks
-import kotlinx.android.synthetic.main.activity_main.*
-import pub.devrel.easypermissions.EasyPermissions
 import tw.com.andyawd.andyawdlibrary.AWDLog
-import tw.com.andyawd.andyawdlibrary.AWDPermissionsFailAlertDialog
-import tw.com.andyawd.andyawdlibrary.AWDToastMgr
-import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+class MainActivity : AppCompatActivity() {
 
     private val mainMenuFragment = MainMenuFragment()
 
@@ -42,16 +33,6 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     @SuppressLint("CheckResult")
     private fun initListener() {
-        ibAmStartRecord.clicks().throttleFirst(BaseConstants.CLICK_TIMER, TimeUnit.MILLISECONDS)
-            .subscribe {
-                createVideoRecord()
-            }
-
-        ibAmEndRecord.clicks().throttleFirst(BaseConstants.CLICK_TIMER, TimeUnit.MILLISECONDS)
-            .subscribe {
-                crglsvAmVideo.endRecording()
-            }
-
         mainMenuFragment.fragmentOpenListener = fragmentOpenListener
     }
 
@@ -60,83 +41,17 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
             .commitAllowingStateLoss()
     }
 
-    private fun createVideoRecord() {
-        val videoRecordPermission = arrayOf(
-            Manifest.permission.CAMERA,
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-
-        if (!EasyPermissions.hasPermissions(this, *videoRecordPermission)) {
-            EasyPermissions.requestPermissions(
-                this,
-                resources.getString(R.string.no_video_record_permission),
-                BaseConstants.VIDEO_RECORD_PERMISSION_RESULT,
-                *videoRecordPermission
-            )
-            return
-        }
-
-        crglsvAmVideo.presetCameraForward(true)
-        crglsvAmVideo.setFitFullView(true)
-        crglsvAmVideo.startRecording(
-            FileManager.instance.getUriFromFile(
-                BaseApplication.context(),
-                Environment.DIRECTORY_MOVIES,
-                "1090504",
-                FileManager.MP4
-            ).toString()
-        )
-    }
-
     private val fragmentOpenListener = object : FragmentOpenListener() {
-        override fun onOpenVideoRecordView() {
-            super.onOpenVideoRecordView()
+        override fun onOpenDuelScore() {
+            super.onOpenDuelScore()
 
-            AWDLog.d("onOpenVideoRecordView")
+            AWDLog.d("onOpenDuelScore")
         }
 
-        override fun onOpenVideoListView() {
-            super.onOpenVideoListView()
+        override fun onOpenHistoryRecord() {
+            super.onOpenHistoryRecord()
 
-            AWDLog.d("onOpenVideoListView")
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            AWDPermissionsFailAlertDialog(this, perms)
-        } else {
-            AWDToastMgr.init(this).build()
-                .show(resources.getString(R.string.open_video_record_permission))
-        }
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-        AWDLog.d("onPermissionsGranted requestCode: $requestCode")
-        when (requestCode) {
-            BaseConstants.VIDEO_RECORD_PERMISSION_RESULT -> {
-                createVideoRecord()
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        AWDLog.d("onActivityResult: $requestCode / resultCode: $resultCode")
-        when (requestCode) {
-            BaseConstants.VIDEO_RECORD_PERMISSION_RESULT -> {
-                createVideoRecord()
-            }
+            AWDLog.d("onOpenHistoryRecord")
         }
     }
 }
